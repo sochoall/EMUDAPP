5d28e741073848d3eb9818bb8e5605027043d2b3
JohnCalle <john_ger1996@hotmail.com>
2019-11-14 09:48:41 -0500

    control de accesoRolMaster

--- web/emovweb/rolMaster.php
+++ web/emovweb/rolMaster.php
@@ -1,27 +1,35 @@
  <?php
-    session_start();
-    if (isset($_SESSION['id']) && isset($_SESSION['rol'])) {
-        $id = $_SESSION['id'];
-        $rol = $_SESSION['rol'];
-		$menu=$_SESSION['menu'];
-        echo " <script type='text/javascript'>
-					window.onload=function() {
+	session_start();
+	if (isset($_SESSION['id']) && isset($_SESSION['rol'])) {
+		if($_SESSION['rol'] == "EMOV")
+		{
+			$id = $_SESSION['id'];
+			$rol = $_SESSION['rol'];
+			$menu=$_SESSION['menu'];
+			echo " <script type='text/javascript'>
+						window.onload=function() {
 
-						document.getElementById('rol').innerHTML ='ROL - $rol';
-						document.getElementById('btncerrar').style.display = 'block';
+							document.getElementById('rol').innerHTML ='ROL - $rol';
+							document.getElementById('btncerrar').style.display = 'block';
 
-						let elementos=document.getElementsByClassName('boton');
-						for(let i=0;i<elementos.length;i++)
-						{
-							elementos[i].addEventListener('click',obtenerValores);
-						} 
-						document.getElementById('tituloModal').innerHTML = 'ROL';
-					}
-			 </script>
-	 ";
-    } else {
-        header('Location: ./');
-    }   
+							let elementos=document.getElementsByClassName('boton');
+							for(let i=0;i<elementos.length;i++)
+							{
+								elementos[i].addEventListener('click',obtenerValores);
+							} 
+							document.getElementById('tituloModal').innerHTML = 'ROL';
+						}
+					</script>
+			";
+		}
+		else{
+			header('Location: ./');
+		}
+		
+	} else {
+		header('Location: ./');
+	} 
+      
 
 ?>
 
