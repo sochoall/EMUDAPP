dd10a9368c0c808fb709316a8fb9a8efdb24d31e
JimmyReinerio <saltojimmy@gmail.com>
2019-11-22 14:41:47 -0500

    arrelos Juan

--- web/emovweb/funcionarioEditar.php
+++ web/emovweb/funcionarioEditar.php
@@ -1,73 +1,73 @@
 <?php include 'header.php'; 
 	 include 'codigophp/sesion.php';
 	 $menu=Sesiones("EMOV");
-	 include 'modalFuncionario.php';
+	 include 'funcionario_modal_selec_institucion.php';
 ?>
 
 <div class="container text-left mt-2">
 	<div class="row">
-    	<div class="col-md-6 offset-md-2">
+    	<div class="col-md-6 offset-md-3">
 			<div class="card">
-      			<h3 class="card-header cyan white-text text-uppercase font-weight-bold text-center" id="titulo"></h3>
+      			<h3 class="card-header cyan white-text font-weight-bold text-center" id="titulo"></h3>
       			<div class="card-body">
 						<div class="container">
 	<div class="row mt-1">
     <div class="col">
 	<form>	
 		<div class="row">
-		<label class="col-sm-4 col-form-label" >cedula:<span style="color:red" >*</span></label>
+		<label class="col-sm-3 col-form-label" >Cédula:<span style="color:red" >*</span></label>
 			<div class="col-sm-5">
 				<input type='text' onblur="jj()" id= "ced" class="form-control text-upperCase form-control-sm"  maxlength="10"/>
 			</div>
 		</div>
 		<div class=" row">
-			<label class="col-sm-4 col-form-label">nombre:<span style="color:red" >*</span></label>
+			<label class="col-sm-3 col-form-label">Nombre:<span style="color:red" >*</span></label>
 			<div class="col-sm-8">
 				<input type='text' onkeyup="this.value = this.value.toUpperCase();" id= "nom" class="form-control form-control-sm"  maxlength="100"/>
 			</div>
 		</div>
 		<div class="row">
-			<label class="col-sm-4 col-form-label">apellido:<span style="color:red" >*</span></label>
+			<label class="col-sm-3 col-form-label">Apellido:<span style="color:red" >*</span></label>
 			<div class="col-sm-8">
 				<input type='text' onkeyup="this.value = this.value.toUpperCase();" id= "ape" class="form-control text-upperCase form-control-sm"  maxlength="50" />
 			</div>
 		</div>
 		<div class="row">
-			<label class="col-sm-4 col-form-label" >direccion:<span style="color:red" >*</span></label>
+			<label class="col-sm-3 col-form-label" >Dirección:<span style="color:red" >*</span></label>
 			<div class="col-sm-8">
 				<input type='text' onkeyup="this.value = this.value.toUpperCase();" id="dir"  class="form-control form-control-sm" maxlength="100" />
 			</div>
 		</div>
 		<div class="row">
-			<label class="col-sm-4 col-form-label">telefono:</label>
+			<label class="col-sm-3 col-form-label">Teléfono:</label>
 			<div class="col-sm-5">
 				<input type='text'id= "telf" class="form-control form-control-sm" maxlength="10"/>
 			</div>
 		</div>	
 		<div class="row">
-			<label class="col-sm-4 col-form-label">celular:<span style="color:red" >*</span></label>
+			<label class="col-sm-3 col-form-label">Celular:<span style="color:red" >*</span></label>
 			<div class="col-sm-5">
 				<input type='text' pattern="\d*" id= "cel" class="form-control form-control-sm" maxlength="10"/>
 			</div>
 		</div>	
 		<div class="row">
-			<label class="col-sm-4 col-form-label">correo:<span style="color:red" >*</span></label>
-			<div class="col-sm-5">
+			<label class="col-sm-3 col-form-label">Correo:<span style="color:red" >*</span></label>
+			<div class="col-sm-8">
 				<input type='text'id= "email" class="form-control form-control-sm" maxlength="100"/>
 			</div>
 		</div>	
 		<div class="row">
-			<label class="col-sm-4 col-form-label">Estado:</label>
-			<div class="col-sm-4">
+			<label class="col-sm-3 col-form-label">Estado:</label>
+			<div class="col-sm-5">
 				<SELECT id="est" class="browser-default custom-select"> 
-					<OPTION VALUE="1" >Activo</OPTION>
-					<OPTION VALUE="0">Inactivo</OPTION>
+					<OPTION VALUE="1" >ACTIVO</OPTION>
+					<OPTION VALUE="0">INACTIVO</OPTION>
 				</SELECT> 
 			</div>
 		</div>
-		<h5 class="container text-center mt-3 text-uppercase"><strong >Datos de la Instituci&oacute;n </strong></h5>
+		<h5 class="container text-center mt-3 text-uppercase"><strong >Datos de la Institución </strong></h5>
 				<div class=" row">
-						<label class="col-md-4  col-form-label align-self-center">C&oacute;digo:<span style="color:red" >*</span></label>
+						<label class="col-md-3  col-form-label align-self-center">Código:<span style="color:red" >*</span></label>
 						<div class="col-md-3 align-self-center">
 							<input type='text' id= "idfun" class="form-control form-control-sm " />
 						</div>
@@ -78,21 +78,18 @@
 					</div>
 					
 						<div class="row ">
-					<label class="col-md-4 col-form-label  align-self-center">Nombre:</label>
+					<label class="col-md-3 col-form-label  align-self-center">Nombre:</label>
 					<div class="col-md-8 align-self-center">
 						<input type="text" id= "chofer" class="form-control text-uppercase form-control-sm" readonly disabled />
 					</div>
 							
 				</div>
-				<div class=" row justify-content-center">
-						<input class="btn cyan" onclick="IngMod(this)" type="submit" value="" id="metodo" name="metodo"/>		
-					<input type="button" value="Cancelar" class="btn cyan" onclick="location.href = 'funcionario.php';"/>
+
+				<div class="text-white row justify-content-center mt-3">
+					<input class="btn cyan" onclick="IngMod(this)" type="submit" value="" id="metodo" name="metodo"/>		
+					<input type="button" value="Cancelar" class="btn cyan" onclick="location.href = 'funcionario.php';"/><br/>
 				</div>
 		
-				<div class="row justify-content-center">
-				<span style="color:red" >* campos obligatorios</span>
-				</div>
-			
 	</form>
 	
 	</div></div></div>
@@ -104,7 +101,6 @@
       	</div>
 	</div>
 </div>
-<div class="mt-5" id="alerta"></div>	
 	<?php
 	$id=0;
 	if (isset($_GET['metodo'])) {
@@ -115,7 +111,8 @@
 	
 			 echo "<script language='javascript'> 
 				document.getElementById('metodo').value ='$metodo';
-				document.getElementById('titulo').innerHTML = ' Funcionario';	
+				document.getElementById('est').disabled=true;	
+				document.getElementById('titulo').innerHTML = 'FUNCIONARIO';	
 				
 			 </script>";
 		}else{			
@@ -123,7 +120,7 @@
 					
 			echo "<script language='javascript'> 
 			document.getElementById('metodo').value ='$metodo';
-			document.getElementById('titulo').innerHTML = '$metodo funcionario';
+			document.getElementById('titulo').innerHTML = 'FUNCIONARIO';
 			fetch('http://localhost:8888/funcionario/$id')
 			  .then(response => response.json())
 			  .then(data => {		  	
@@ -140,12 +137,8 @@
 			  .catch(error => console.log('error al cargar datos'))
 			  
 			</script>";
-		}			
-		
+		}		
 	}
-	
-	
-
 ?>
 
 
