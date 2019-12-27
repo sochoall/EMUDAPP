7d80bd384343a346aa357eba982f409abb0f221f
LojaPablo <lpabloloja@hotmail.com>
2019-12-27 16:19:32 -0500

    Optimizacion de Codigo

--- app_movil/lib/representante/widgets/lista_objetos_estudiante.dart
+++ app_movil/lib/representante/widgets/lista_objetos_estudiante.dart
@@ -1,26 +1,16 @@
-import 'package:app_movil/representante/widgets/lista_recorrido_estudiante.dart';
 import 'package:flutter/material.dart';
-
 import '../../main.dart';
 import '../../provider.dart';
 import 'lista_objetos_recorrido_estudiante.dart';
 
 class ListaEstudiantesObjetos extends StatelessWidget {
-  //Creacion de Lista de rutas
   @override
-
-   final String id_usuario; //Creacion de varibales que se vayan a usar
-
-
+  final String id_usuario; 
   ListaEstudiantesObjetos(this.id_usuario);
-
-
   Widget build(BuildContext context) {
     return FutureBuilder(
         future: listaEstudiante.cargarData4(id_usuario),
-        //Enlazada al cargardata
         initialData: [],
-        //Informcion por defecto
         builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
           return CustomScrollView(
             slivers: datosSliverList(snapshot.data, context),
@@ -48,19 +38,12 @@ List<Widget> datosSliverList(List<dynamic> datos, BuildContext context) {
 
 List<Widget> datosListTile(List<dynamic> datos, BuildContext context) {
   final List<Widget> opciones = [];
-
   datos.forEach(
     (opt) {
       final widgetTemp = ExpansionTile(
         title: Text(opt['nombre']+" "+opt['apellido'], style: TextStyle(
                               fontWeight: FontWeight.bold),),
-        //subtitle: Text(opt['descripcion']),
-        
         leading: Icon(Icons.directions_bus),
-        //onTap: (){
-        //Navigator.of(context).push(
-        //MaterialPageRoute(builder: (BuildContext contexto) => PantallaParadas(opt['nombre'])));
-        //},
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
         children: <Widget>[
           Container(
